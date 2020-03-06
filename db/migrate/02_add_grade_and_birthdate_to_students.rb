1d1768af2e52b12ec89f113181ca1ad3509b4e9f
George Blackmore <gtblackmore@gmail.com>
2020-03-06 03:52:52 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -2,7 +2,7 @@ class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.2]
   def change
     add_column :students do |c|
       c.integer :grade,
-      c.string :birthdate,
+      c.string birthdate,
     end
   end
 end
